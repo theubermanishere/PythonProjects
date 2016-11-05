88bbb8204fda6a9a8848980bd2698c5b5412ba60
theubermanishere <vinayakkaushikk@gmail.com>
2016-11-05 15:49:23 +0530

    fizzbuzz solved

---
+++ stuff/fizzbuzz.py
@@ -0,0 +1,12 @@
+for i in range(100):
+    if i%3==0:
+        if i%5==0:
+            print('fizzbuzz\n')
+            continue
+        else:
+            print('fizz\n')
+            continue
+    if i%5==0:
+        print('buzz\n')
+        continue
+    print(i,'\n')
